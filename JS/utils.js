b9022cfce43683953232a57ceafa46c4672eb94c
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-06 15:35:54 +0530

    Created utils.js code

--- JS/utils.js
+++ JS/utils.js
@@ -1,1 +1,44 @@
+function generateId() {
 
+    return Date.now();
+
+}
+
+function getTeacherById(id) {
+
+    return teachers.find(teacher => teacher.id === id);
+
+}
+
+function getTaskById(id) {
+
+    return tasks.find(task => task.id === id);
+
+}
+
+function countTasks(status) {
+
+    return tasks.filter(task => task.status === status).length;
+
+}
+
+function formatDate(date) {
+
+    return new Date(date).toLocaleDateString();
+
+}
+
+function today() {
+
+    return new Date().toISOString().split("T")[0];
+
+}
+
+function isOverdue(task) {
+
+    if (task.status === "Completed")
+        return false;
+
+    return task.deadline < today();
+
+}
