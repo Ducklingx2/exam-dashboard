b3789dc774d7a6b26ec5f7be67c6fcafc00b57ee
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-07 18:12:52 +0530

    Made backend

--- Backend/main.go
+++ Backend/main.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.Handle("/", http.FileServer(http.Dir("../frontend")))
+
+	log.Println("Server running on http://localhost:8080")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
