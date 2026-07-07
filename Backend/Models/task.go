7adae7c7091abe06023a3a809609af25e7ec9304
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-07 19:53:29 +0530

    Wrote task.go code

--- Backend/Models/task.go
+++ Backend/Models/task.go
@@ -1,1 +1,9 @@
+package models
 
+type Task struct {
+	ID        int    `json:"id"`
+	TeacherID int    `json:"teacherId"`
+	Title     string `json:"title"`
+	Deadline  string `json:"deadline"`
+	Status    string `json:"status"`
+}
